4b8dfb22e41d56cc5b479034a054e8d89bec36ea
FabioLutz <fabio_lutz@outlook.com>
2025-01-02 20:30:24 -0300

    feat: change Markdown content from string to file
    Altera conteúdo do Markdown vir de uma string para vir de um arquivo.

--- script/converter.mjs
+++ script/converter.mjs
@@ -1,10 +1,14 @@
 import markdownIt from 'markdown-it';
+import fs from 'node:fs';
+import path from 'node:path';
+import url from 'node:url';
 
-const markdownContent = `
-# Texto markdown
+const scriptDir = path.dirname(url.fileURLToPath(import.meta.url));
+const rootPath = path.dirname(scriptDir);
+const markdownPath = path.join(rootPath, 'markdown');
 
-Texto com **negrito** e _itálico_.
-`;
+const markdownFile = path.join(markdownPath, 'file.md');
+const markdownContent = fs.readFileSync(markdownFile, 'utf8');
 
 const htmlContent = markdownIt().render(markdownContent);
 
