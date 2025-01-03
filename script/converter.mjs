a45f9c6f1ac3130bc0dfedcc5d870070be30f195
FabioLutz <fabio_lutz@outlook.com>
2025-01-03 00:17:49 -0300

    feat: change output from console to HTML file
    Altera a saída do conteúdo do console para um arquivo HTML.

--- script/converter.mjs
+++ script/converter.mjs
@@ -6,10 +6,13 @@ import url from 'node:url';
 const scriptDir = path.dirname(url.fileURLToPath(import.meta.url));
 const rootPath = path.dirname(scriptDir);
 const markdownPath = path.join(rootPath, 'markdown');
+const publicDir = path.join(rootPath, 'public');
 
 const markdownFile = path.join(markdownPath, 'file.md');
+const outputFile = path.join(publicDir, 'index.html');
+
 const markdownContent = fs.readFileSync(markdownFile, 'utf8');
 
 const htmlContent = markdownIt().render(markdownContent);
 
-console.log(htmlContent);
+fs.writeFileSync(outputFile, htmlContent, 'utf8');
