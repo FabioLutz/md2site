9abd67d57eeddf017bb8faa1014a82eee2c2523a
FabioLutz <fabio_lutz@outlook.com>
2025-01-03 21:21:14 -0300

    refactor: change list of paths from multiple consts to object
    Altera várias constantes em apenas um objeto para melhor organização e
    legibilidade.

--- script/converter.mjs
+++ script/converter.mjs
@@ -3,13 +3,19 @@ import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
 
-const scriptDir = path.dirname(url.fileURLToPath(import.meta.url));
-const rootPath = path.dirname(scriptDir);
-const markdownPath = path.join(rootPath, 'markdown');
-const publicDir = path.join(rootPath, 'public');
+const rootPath = path.join(
+    path.dirname(url.fileURLToPath(import.meta.url)),
+    '..'
+);
 
-const markdownFile = path.join(markdownPath, 'file.md');
-const outputFile = path.join(publicDir, 'index.html');
+const paths = {
+    script: path.join(rootPath, 'script'),
+    markdown: path.join(rootPath, 'markdown'),
+    public: path.join(rootPath, 'public'),
+};
+
+const markdownFile = path.join(paths.markdown, 'file.md');
+const outputFile = path.join(paths.public, 'index.html');
 
 const markdownContent = fs.readFileSync(markdownFile, 'utf8');
 
