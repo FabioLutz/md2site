bb399850cf4084eb8b3412148bc6ee5a55c7a3a6
FabioLutz <fabio_lutz@outlook.com>
2025-01-12 19:08:59 -0300

    feat: add styles to templates
    Possibilita adicionar estilos personalizados para os templates de
    maneira simples.

--- script/converter.mjs
+++ script/converter.mjs
@@ -12,12 +12,16 @@ const paths = {
     script: path.join(rootPath, 'script'),
     configFile: path.join(rootPath, 'config.json'),
     markdown: path.join(rootPath, 'markdown'),
-    public: path.join(rootPath, 'public'),
     template: path.join(rootPath, 'template'),
+    style: path.join(rootPath, 'template', 'style'),
+    public: path.join(rootPath, 'public'),
+    publicStyle: path.join(rootPath, 'public', 'style'),
 };
 
 const configContent = fs.readFileSync(paths.configFile);
 const config = JSON.parse(configContent.toString());
+
+fs.cpSync(paths.style, paths.publicStyle, { recursive: true });
 
 config.pages.forEach(page => {
     const markdownFile = path.join(paths.markdown, page.file);
