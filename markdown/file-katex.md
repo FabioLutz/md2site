29dcc7f63b24d8a150064913c16e862010fa4ed3
FabioLutz <fabio_lutz@outlook.com>
2025-02-02 21:19:45 -0300

    feat: add latex support

---
+++ markdown/file-katex.md
@@ -0,0 +1,11 @@
+# KaTeX teste
+
+## Linha
+
+<div class="latex-inline">\sqrt{x^2 + y^2}</div>
+
+## Bloco
+
+<div class="latex-block">
+    \sqrt{x^2 + y^2}
+</div>
--- script/converter.mjs
+++ script/converter.mjs
@@ -1,7 +1,12 @@
-import markdownIt from 'markdown-it';
+import katex from 'katex';
+import md from 'markdown-it';
 import fs from 'node:fs';
 import path from 'node:path';
 import url from 'node:url';
+
+const markdownIt = new md({
+    html: true,
+});
 
 const rootPath = path.join(
     path.dirname(url.fileURLToPath(import.meta.url)),
@@ -11,20 +16,32 @@ const rootPath = path.join(
 const paths = {
     script: path.join(rootPath, 'script'),
     configFile: path.join(rootPath, 'config.json'),
+
     markdown: path.join(rootPath, 'markdown'),
     image: path.join(rootPath, 'markdown', 'image'),
+
     template: path.join(rootPath, 'template'),
     templateStyle: path.join(rootPath, 'template', 'style'),
     templateFavicon: path.join(rootPath, 'template', 'favicon'),
+
+    katexJs: path.join(rootPath, 'node_modules', 'katex', 'dist', 'katex.min.js'),
+    katexCss: path.join(rootPath, 'node_modules', 'katex', 'dist', 'katex.min.css'),
+
     static: path.join(rootPath, 'static-html'),
     staticStyle: path.join(rootPath, 'static-html', 'style'),
     staticFavicon: path.join(rootPath, 'static-html', 'favicon'),
+
     public: path.join(rootPath, 'public'),
+
     publicStyle: path.join(rootPath, 'public', 'style'),
     publicFavicon: path.join(rootPath, 'public', 'favicon'),
     publicImage: path.join(rootPath, 'public', 'image'),
+
     publicTemplateStyle: path.join(rootPath, 'public', 'style', 'template'),
     publicTemplateFavicon: path.join(rootPath, 'public', 'favicon', 'template'),
+
+    publicKatexJs: path.join(rootPath, 'public', 'node_modules', 'katex', 'dist', 'katex.min.js'),
+    publicKatexCss: path.join(rootPath, 'public', 'node_modules', 'katex', 'dist', 'katex.min.css'),
 };
 
 const configContent = fs.readFileSync(paths.configFile);
@@ -42,14 +59,43 @@ copyFiles(paths.image, paths.publicImage);
 copyFiles(paths.static, paths.public);
 copyFiles(paths.staticStyle, paths.publicStyle);
 copyFiles(paths.staticFavicon, paths.publicFavicon);
+copyFiles(paths.katexJs, paths.publicKatexJs);
+copyFiles(paths.katexCss, paths.publicKatexCss);
+
+function renderLatex(content) {
+    const inlineRegex = /<div class="latex-inline">(.*?)<\/div>/g;
+    const blockRegex = /<div class="latex-block">(.*?)<\/div>/gs;
+
+    content = content.replace(inlineRegex, (_, equation) => katex.renderToString(
+        equation.trim(),
+        {
+            displayMode: false,
+            output: 'html',
+        }
+    )
+    );
+
+    content = content.replace(blockRegex, (_, equation) => katex.renderToString(
+        equation.trim(),
+        {
+            displayMode: true,
+            output: 'html',
+        }
+    )
+    );
+
+    return content;
+}
 
 config.pages.forEach(page => {
     const markdownFile = path.join(paths.markdown, page.file);
     const templateFile = path.join(paths.template, page.template);
     const outputFile = path.join(paths.public, page.output);
 
-    const content = fs.readFileSync(markdownFile, 'utf8');
-    const htmlContent = markdownIt().render(content);
+    const plainTextContent = fs.readFileSync(markdownFile, 'utf8');
+    const pureHtml = markdownIt.render(plainTextContent);
+
+    const htmlContent = renderLatex(pureHtml);
 
     const outputDir = path.dirname(outputFile);
     if (!fs.existsSync(outputDir)) {
@@ -63,13 +109,13 @@ config.pages.forEach(page => {
         content: htmlContent,
         basePath: basePath ? `${basePath}` : '.',
     };
-    
+
     const templateContent = fs.readFileSync(templateFile, 'utf8');
-    
+
     const finalHtml = templateContent.replace(
         /{{(\w+)}}/g,
         (match, placeholder) => replacements[placeholder] || match
     );
-    
+
     fs.writeFileSync(outputFile, finalHtml, 'utf8');
 });
